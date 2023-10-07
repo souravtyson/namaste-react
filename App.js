e55c42df2444657c7d4ac10262ee79bbfc212c9a
sourav suman <souravtyson@gmail.com>
2023-10-07 18:12:01 +0530

    episode 4

--- App.js
+++ App.js
@@ -1,98 +1,64 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM  from "react-dom/client";
 
-
-// const parent = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child", key: "child0" }, [
-//     React.createElement("h1", { key: 0 }, "this is h1 tag"),
-//     React.createElement("h2", { key: 1 }, "this is h2 tag"),
-//   ]),
-//   React.createElement("div", { id: "child2", key: "child1" }, [
-//     React.createElement("h1", { key: 0 }, "this is h1 tag"),
-//     React.createElement("h2", { key: 1 }, "this is h2 tag"),
-//   ]),
-// ]);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(parent);
-// console.log(root);
-// root.render(parent);
-
-
-// Coding Assignment point 1
-
-                    //   const element = React.createElement("div", {}, [
-                    //     React.createElement("h1", { class: "title"}, "this is h1 tag"),
-                    //     React.createElement("h2", {class: "title"}, "this is h2 tag"),
-                    //     React.createElement("h3", {class: "title"}, "this is h3 element")
-                    //   ])
-
-                    //   const root = ReactDOM.createRoot(document.getElementById("root"));
-                    //   root.render(element)
-
-// Coding Assignment point 2
-
-                    //   const elem = <div>
-                    //     <h1 className="title">this is h1 tag</h1>
-                    //     <h2 className="title">this is h2 tag</h2>
-                    //     <h3 className="title">this is h3 tag</h3>
-                    //   </div> 
-
-                    //   const root = ReactDOM.createRoot(document.getElementById("root"))
-                    //   root.render(elem)
-
-// Coding Assignment point 3 and 4
-
-                    //   const Elem = ({id}) => {
-                    //     return (
-                    //       <div>
-                    //         <h1 className="title">this is h1 tag {id}</h1>
-                    //         <h2 className="title">this is h2 tag {id}</h2>
-                    //         <h3 className="title">this is h3 tag {id}</h3>
-                    //       </div> 
-                    //     )
-                    //   }
-
-                    //   const root = ReactDOM.createRoot(document.getElementById("root"))
-                    //   root.render(<Elem id="2"/>)
-
-// Coding Assignment point 5
-
-// const Child = () => {
-//   return (
-//     <div>
-//       <h3>Inside child component</h3>
-//     </div>
-//   )
-// }
-
-// const Parent = () => {
-//   return(
-//     <div>
-//       <p>This is component composition example</p>
-//       {Child()}
-//     </div>
-//   )
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(<Parent/>)
-
-
-// Coding assignment point 6
 
 const Header = () => {
-    return (<div className="container">
-        <div className="logo-container">
-            <img className="logo" alt="logo" src="https://previews.123rf.com/images/miracel123/miracel1231801/miracel123180100797/94312688-food-delivery-logo.jpg" />
+    return (
+        <div className="header-container">
+            <div className="logo-container">
+                <img className="logo" src="https://previews.123rf.com/images/miracel123/miracel1231801/miracel123180100797/94312688-food-delivery-logo.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
-        <div className="search-align">
-            Search: <input type="text" placeholder="Search"/>
-        </div>
-        <div>
-            <img className="profile" alt="logo" src="https://www.citypng.com/public/uploads/preview/white-user-member-guest-icon-png-image-31634946729lnhivlto5f.png" />
-        </div>
-    </div>)
+    )
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Header />)
+const RestaurantCard = () => {
+    return (
+        <div className="card-container" style={{backgroundColor: "#f0f0f0"}}>
+            <img className="rest-img" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvo1djhbwrgfqd64k0tl"/>
+            <div style={{fontSize: 12}}>
+                <h3 style={{margin: 3}}>Meghana Biryani</h3>
+                <h5 style={{margin: 1}}>Biryani, North Indianm Asian</h5>
+                <h5 style={{margin: 0}}>4.4 Stars</h5>
+                <h5 style={{margin: 0}}>38 minutes</h5>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="restaurant-container">
+            <div className="serach-restaurant">
+                <label for="search">Search : </label>
+                <input type="text" id="search"/>
+            </div>
+            <div className="restaurant-list">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout />)
