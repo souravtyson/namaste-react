28c26761e67ca8befb51d710e1a17100980bbfa6
souravtyson <souravtyson@gmail.com>
2023-10-07 15:25:35 +0000

    ep4 completed

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+import {data as swiggyRestaurantList} from './mocks/mocky.js'
 
 
 const Header = () => {
@@ -20,20 +21,26 @@ const Header = () => {
     )
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+
+    const {restaurant} = props
+    const { name: restaurantName, cuisines: cuisine, avgRating: starRating, cloudinaryImageId: restaurantImage, costForTwo} = restaurant.info;
+
     return (
         <div className="card-container" style={{backgroundColor: "#f0f0f0"}}>
-            <img className="rest-img" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvo1djhbwrgfqd64k0tl"/>
+            <img className="rest-img" alt="res-logo" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurantImage}`}/>
             <div style={{fontSize: 12}}>
-                <h3 style={{margin: 3}}>Meghana Biryani</h3>
-                <h5 style={{margin: 1}}>Biryani, North Indianm Asian</h5>
-                <h5 style={{margin: 0}}>4.4 Stars</h5>
-                <h5 style={{margin: 0}}>38 minutes</h5>
+                <h3 style={{margin: 3}}>{restaurantName}</h3>
+                <h5 style={{margin: 1}}>{cuisine.join(", ")}</h5>
+                <h5 style={{margin: 1}}>{starRating} Stars</h5>
+                <h5 style={{margin: 1}}>{costForTwo}</h5>
+                <h5 style={{margin: 1}}>{restaurant.info.sla.deliveryTime} min</h5>
             </div>
         </div>
     )
 }
 
+//                /* {   card.id=restaurant_grid_listing */}
 const Body = () => {
     return (
         <div className="restaurant-container">
@@ -41,11 +48,13 @@ const Body = () => {
                 <label for="search">Search : </label>
                 <input type="text" id="search"/>
             </div>
+
             <div className="restaurant-list">
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                {
+                    swiggyRestaurantList.card.gridElements.infoWithStyle.restaurants.map((restaurant) => <RestaurantCard key={restaurant.info.id} restaurant={restaurant}/>)
+                }
+                {/* <RestaurantCard restaurantName="Meghana Biryani" cuisine="Biryani, North Indianm Asian" starRating="4.4 Stars" deliveryTime="38 minutes" restaurantImage="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvo1djhbwrgfqd64k0tl"/>
+                <RestaurantCard restaurantName="Meghana Biryani" cuisine="Biryani, North Indianm Asian" starRating="4.4 Stars" deliveryTime="38 minutes" restaurantImage="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvo1djhbwrgfqd64k0tl"/> */}
             </div>
         </div>
     )
