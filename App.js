0bc37820cc7f6f9082f09ce17533d5e41c84b4a7
souravtyson <souravtyson@gmail.com>
2023-10-07 16:23:52 +0000

    removed comments

--- App.js
+++ App.js
@@ -53,8 +53,6 @@ const Body = () => {
                 {
                     swiggyRestaurantList.card.gridElements.infoWithStyle.restaurants.map((restaurant) => <RestaurantCard key={restaurant.info.id} restaurant={restaurant}/>)
                 }
-                {/* <RestaurantCard restaurantName="Meghana Biryani" cuisine="Biryani, North Indianm Asian" starRating="4.4 Stars" deliveryTime="38 minutes" restaurantImage="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvo1djhbwrgfqd64k0tl"/>
-                <RestaurantCard restaurantName="Meghana Biryani" cuisine="Biryani, North Indianm Asian" starRating="4.4 Stars" deliveryTime="38 minutes" restaurantImage="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvo1djhbwrgfqd64k0tl"/> */}
             </div>
         </div>
     )
