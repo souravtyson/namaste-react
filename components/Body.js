bddb67e6f905d4dafb52d2e14de3a4b4eaa59d34
souravtyson <souravtyson@gmail.com>
2023-11-15 07:20:05 +0000

    search and top rated restaurant functionality added

--- components/Body.js
+++ components/Body.js
@@ -1,28 +1,38 @@
-import {data as swiggyRestaurantList} from '../mocks/mocky.js'
-import RestaurantCard from './RestaurantCard.js';
-
-
+import { useState } from "react";
+import { data as mockSwiggyRestaurantList } from "../mocks/mocky.js";
+import RestaurantCard from "./RestaurantCard.js";
 
 const Body = () => {
-    return (
-        <div className="restaurant-container">
-            <div className="serach-restaurant">
-                <label htmlFor="search">Search : </label>
-                <input type="text" id="search"/>
-            </div>
+  const [swiggyRestaurantList, setSwiggyRestaurantList] = useState(
+    mockSwiggyRestaurantList.card.gridElements.infoWithStyle.restaurants
+  );
+  const [searchRes, setSearchRes] = useState("")
 
-            <div className="restaurant-list">
-                {
-                    swiggyRestaurantList
-                    .card
-                    .gridElements
-                    .infoWithStyle
-                    .restaurants
-                    .map((restaurant) => <RestaurantCard key={restaurant.info.id} restaurant={restaurant}/>)
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="restaurant-container">
+      <div className="serach-restaurant">
+        <label htmlFor="search">Search : </label>
+        <input type="text" id="search" value={searchRes} onChange={(e) => setSearchRes(e.target.value)}/>
+        <button onClick={() => {
+          let x = swiggyRestaurantList.filter(res => res.info.name.toLowerCase().includes(searchRes.toLowerCase()))
+          setSwiggyRestaurantList(x)
+        }}>Search</button>
+        <button
+          onClick={() => {
+            setSwiggyRestaurantList(swiggyRestaurantList.filter(res => res.info.avgRating > 4.2))
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+
+      <div className="restaurant-list">
+        {swiggyRestaurantList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,22 +1,31 @@
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
+  const { restaurant } = props;
+  const {
+    name: restaurantName,
+    cuisines: cuisine,
+    avgRating: starRating,
+    cloudinaryImageId: restaurantImage,
+    costForTwo,
+  } = restaurant.info;
 
-    const {restaurant} = props
-    const { name: restaurantName, cuisines: cuisine, avgRating: starRating, cloudinaryImageId: restaurantImage, costForTwo} = restaurant.info;
+  return (
+    <div className="card-container" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="rest-img"
+        alt="res-logo"
+        src={`${CDN_URL}${restaurantImage}`}
+      />
+      <div style={{ fontSize: 12 }}>
+        <h3 style={{ margin: 3 }}>{restaurantName}</h3>
+        <h5 style={{ margin: 1 }}>{cuisine.join(", ")}</h5>
+        <h5 style={{ margin: 1 }}>{starRating} Stars</h5>
+        <h5 style={{ margin: 1 }}>{costForTwo}</h5>
+        <h5 style={{ margin: 1 }}>{restaurant.info.sla.deliveryTime} min</h5>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="card-container" style={{backgroundColor: "#f0f0f0"}}>
-            <img className="rest-img" alt="res-logo" src={`${CDN_URL}${restaurantImage}`}/>
-            <div style={{fontSize: 12}}>
-                <h3 style={{margin: 3}}>{restaurantName}</h3>
-                <h5 style={{margin: 1}}>{cuisine.join(", ")}</h5>
-                <h5 style={{margin: 1}}>{starRating} Stars</h5>
-                <h5 style={{margin: 1}}>{costForTwo}</h5>
-                <h5 style={{margin: 1}}>{restaurant.info.sla.deliveryTime} min</h5>
-            </div>
-        </div>
-    )
-}
-
-export default RestaurantCard
+export default RestaurantCard;
